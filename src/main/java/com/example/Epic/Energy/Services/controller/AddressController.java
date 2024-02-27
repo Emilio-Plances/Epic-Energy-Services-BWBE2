911066e89a3e4185d9a284416d0b770dd8f7b09d
FabCaldarini <fabio.caldarini99@gmail.com>
2024-02-27 12:12:51 +0100

    controller push fabiobranch

--- src/main/java/com/example/Epic/Energy/Services/controller/AddressController.java
+++ src/main/java/com/example/Epic/Energy/Services/controller/AddressController.java
@@ -1,7 +1,47 @@
 package com.example.Epic.Energy.Services.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Epic.Energy.Services.entities.Address;
+import com.example.Epic.Energy.Services.exceptions.NotFoundException;
+import com.example.Epic.Energy.Services.services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/services")
 public class AddressController {
+    @Autowired
+    private AddressService addressService;
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        List<Address> addresses = addressService.getAllAddresses();
+        return new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable Long id) throws NotFoundException {
+        Address address = addressService.getAddressById(id);
+        return new ResponseEntity<>(address, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
+        Address newAddress = addressService.createAddress(address);
+        return new ResponseEntity<>(newAddress, HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody Address addressDetails) throws NotFoundException {
+        Address address = addressService.updateAddress(id, addressDetails);
+        return new ResponseEntity<>(address, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAddress(@PathVariable Long id) throws NotFoundException {
+        addressService.deleteAddress(id);
+        String message = "User with ID" + id + " has been deleted";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/example/Epic/Energy/Services/controller/InvoiceController.java
+++ src/main/java/com/example/Epic/Energy/Services/controller/InvoiceController.java
@@ -1,7 +1,45 @@
 package com.example.Epic.Energy.Services.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Epic.Energy.Services.entities.Invoice;
+import com.example.Epic.Energy.Services.exceptions.NotFoundException;
+import com.example.Epic.Energy.Services.services.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/invoices")
 public class InvoiceController {
+    @Autowired
+    private InvoiceService invoiceService;
+    @GetMapping
+    public ResponseEntity<List<Invoice>>getAllInvoices(){
+        List<Invoice>invoices= invoiceService.getAllInvoices();
+        return new ResponseEntity<>(invoices, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Invoice> getInvoiceById(@PathVariable Long id)throws NotFoundException {
+        Invoice invoice=invoiceService.getInvoiceById(id);
+        return new ResponseEntity<>(invoice, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Invoice> createInvoice(@RequestBody Invoice invoice) {
+        Invoice createdInvoice = invoiceService.createInvoice(invoice);
+        return new ResponseEntity<>(createdInvoice, HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Invoice> updateInvoice(@PathVariable Long id, @RequestBody Invoice invoice) throws NotFoundException {
+        Invoice updatedInvoice = invoiceService.updateInvoice(id, invoice);
+        return new ResponseEntity<>(updatedInvoice, HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteInvoice(@PathVariable Long id) throws NotFoundException {
+        invoiceService.deleteInvoice(id);
+        String message = "Invoice with ID" + id + " has been deleted";
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
